6d53dae5c0a04d52f1118ff25198394405799dfa
Tein van der Lugt <teinvdlugt@gmail.com>
2016-03-14 15:48:09 +0100

    Improve some layout and texts

--- app/src/main/java/com/teinproductions/tein/pitrainer/ActivityInterface.java
+++ app/src/main/java/com/teinproductions/tein/pitrainer/ActivityInterface.java
@@ -5,10 +5,7 @@ import android.widget.EditText;
 
 public interface ActivityInterface {
     void preventSoftKeyboardFromShowingUp(EditText et, boolean show);
-
     void vibrate();
-
     void animateToolbarColor(boolean red);
-
     void swapFragment(Class fragmentClass);
 }

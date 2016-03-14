071420e3c6eea0c4438094dfcb526b918cad2c42
Tein van der Lugt <teinvdlugt@gmail.com>
2016-03-14 15:46:26 +0100

    Improve some layout and texts

--- app/src/main/java/com/teinproductions/tein/pitrainer/Game.java
+++ app/src/main/java/com/teinproductions/tein/pitrainer/Game.java
@@ -1,17 +1,12 @@
 package com.teinproductions.tein.pitrainer;
 
-import android.content.Context;
 import android.support.annotation.StringRes;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class Game {
 
-    private @StringRes int name;
+    private
+    @StringRes
+    int name;
     private Class fragment;
 
     public Game(int name, Class fragment) {

8ac0bc4e89c4678581abeb83aa48185f94594567
Tein van der Lugt <teinvdlugt@gmail.com>
2016-11-19 14:58:04 +0100

    Work on moving the keys in CustomiseKeyboardView

--- app/src/main/java/com/teinproductions/tein/pitrainer/keyboard/CustomiseKeyboardView.java
+++ app/src/main/java/com/teinproductions/tein/pitrainer/keyboard/CustomiseKeyboardView.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.Space;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -29,7 +30,6 @@ public class CustomiseKeyboardView extends FrameLayout implements View.OnTouchLi
     private String[] currentLayout = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", " ", "b"};
     private TableLayout tableLayout;
     private TableRow[] tableRows;
-    private Space placeholder;
 
     public void setKeyboardLayout(String[] layout) {
         this.currentLayout = layout;
@@ -60,16 +60,81 @@ public class CustomiseKeyboardView extends FrameLayout implements View.OnTouchLi
         setKeyboardLayout(currentLayout);
     }
 
+    /**
+     * The position of the pixel, with respect to the key TextView,
+     * which is grabbed.
+     */
+    private float touchX, touchY;
+    /**
+     * The position of the key TextView inside this CustomiseKeyboardView.
+     */
+    private float left, top;
+    private View movingView;
+    private static final LayoutParams movingViewParams = new LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    private Space placeholder;
+
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
             view.setSelected(true);
-            return true;
-        } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-            view.setSelected(false);
+            ViewGroup tableRow = (ViewGroup) view.getParent();
+            int index = tableRow.indexOfChild(view);
+            tableRow.removeViewAt(index);
+            tableRow.addView(placeholder, index);
+            touchX = motionEvent.getX();
+            touchY = motionEvent.getY();
+            left = view.getLeft();
+            top = tableRow.getTop();
+            movingView = view;
+            addView(view, movingViewParams);
             return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_MOVE && movingView != null) {
+            movingView.setTranslationX(ev.getX() - touchX);
+            movingView.setTranslationY(ev.getY() - touchY);
+            return false;
+        } else if (ev.getAction() == MotionEvent.ACTION_UP && movingView != null) {
+            movingView.setSelected(false);
+            float x = ev.getX();
+            float y = ev.getY();
+            for (TableRow tableRow : tableRows) {
+                for (int i = 0; i < tableRow.getChildCount(); i++) {
+                    View view = tableRow.getChildAt(i);
+                    int left = view.getLeft();
+                    int right = view.getRight();
+                    int top = tableRow.getTop();
+                    int bottom = tableRow.getBottom();
+                    if (x < right && x > left && y > top && y < bottom) {
+                        TableRow placeholderRow = null;
+                        int placeholderIndex = -1;
+                        for (TableRow tableRow2 : tableRows) {
+                            for (int j = 0; j < tableRow2.getChildCount(); j++) {
+                                if (tableRow2.getChildAt(j).equals(placeholder)) {
+                                    placeholderRow = tableRow2;
+                                    placeholderIndex = j;
+                                }
+                            }
+                        }
+                        if (placeholderRow == null) return false;
+                        placeholderRow.removeViewAt(placeholderIndex);
+                        tableRow.removeViewAt(i);
+                        placeholderRow.addView(view, placeholderIndex);
+                        removeView(movingView);
+                        tableRow.addView(movingView, i);
+                        movingView = null;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return super.onInterceptTouchEvent(ev);
     }
 
     public CustomiseKeyboardView(Context context) {
